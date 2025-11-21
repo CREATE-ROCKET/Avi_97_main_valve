e64cfae51d1cef60356c5db16751a5d00f1d5fe0
Kuh456 <kudog1106@gmail.com>
2025-11-21 14:21:44 +0900

    サーボが1つになったので、片方を消去

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,7 @@
 #define SendingServoAngleId 0x401
 #define ReceivingServoAngleId 0x300
 #define ReceivingServoAngleRequestId 0x301
-#define SendingServoAngleId2 0x402
-#define ReceivingServoAngleId2 0x200
-#define ReceivingServoAngleRequestId2 0x201
 #define ValveOpenId 0x10b
-#define RX 23
-#define TX 22
 #define RXFD 19
 #define TXFD 18
 #define LED 32
@@ -21,11 +16,9 @@
 #define CAN_RX 13
 
 constexpr byte EN_PIN = 17; // 基板21
-constexpr byte EN_PIN2 = 21;
 constexpr long BAUDRATE = 115200;
 constexpr int TIMEOUT = 1000;                                  // 通信できてないか確認用にわざと遅めに設定
 IcsHardSerialClass krs(&Serial2, EN_PIN, BAUDRATE, TIMEOUT);   // インスタンス＋ENピン(17番ピン)およびUARTの指定
-IcsHardSerialClass krs2(&Serial1, EN_PIN2, BAUDRATE, TIMEOUT); // インスタンス＋ENピン(21番ピン)およびUARTの指定
 
 float kakudo_o = 58;  // open 角度
 float kakudo_c = -77; // close 角度
@@ -36,16 +29,6 @@ int pos_r = 0;
 int maeposr = pos_r; /*以下でバウンス*/
 float currentPos = 0;
 float Pos = 0;
-
-float kakudo_o2 = 100 - 128; // open 角度
-float kakudo_c2 = 10 - 128;  // close 角度
-float kakudo_r2 = 0;         // recieve 角度
-int pos_o2 = kakudo_o2 * 8000 / 270 + 7000;
-int pos_c2 = kakudo_c2 * 8000 / 270 + 7000;
-int pos_r2 = 0;
-int maeposr2 = pos_r2; /*以下でデバウンス*/
-float currentPos2 = 0;
-float Pos2 = 0;
 
 long count = 0;
 int count_EMG = 0;
@@ -69,9 +52,7 @@ void setup()
   pinMode(EMG, INPUT);
   // サーボモータの通信初期設定
   Serial2.begin(115200, SERIAL_8N1, RXFD, TXFD);
-  Serial1.begin(115200, SERIAL_8N1, RX, TX);
   krs.begin(); // サーボモータの通信初期設定
-  krs2.begin();
   digitalWrite(LED, HIGH);
   krs.setFree(0);
 }
@@ -86,17 +67,6 @@ void getandsendPos()
   rdata[2] = 16;
   rdata[3] = 13;
   if (CAN.sendData(SendingServoAngleId, rdata, 4))
-  {
-    Serial.println("failed to send CAN data");
-  }
-  currentPos2 = krs2.getPos(0);
-  Pos2 = (currentPos2 - 7000) / 8000 * 270;
-  uint8_t rdata2[4];
-  rdata2[0] = Pos2;
-  rdata2[1] = abs(kakudo_r2);
-  rdata2[2] = 16;
-  rdata2[3] = 13;
-  if (CAN.sendData(SendingServoAngleId2, rdata2, 4))
   {
     Serial.println("failed to send CAN data");
   }
@@ -132,22 +102,6 @@ void loop()
         case ReceivingServoAngleRequestId:
           getandsendPos();
           break;
-        case ReceivingServoAngleId2:
-          kakudo_r2 = message.data[0] - 128;
-          pos_r2 = kakudo_r2 * 8000 / 270 + 7000;
-          if (kakudo_r2 > 64)
-          {
-            digitalWrite(LED, HIGH);
-          }
-          else
-          {
-            digitalWrite(LED, LOW);
-          }
-          Serial.println("m");
-          break;
-        case ReceivingServoAngleRequestId2:
-          getandsendPos();
-          break;
         case ValveOpenId:
           krs.setPos(0, pos_r);
           break;
@@ -160,11 +114,6 @@ void loop()
           flag_free = 1;
         }
 
-        if (maeposr2 != pos_r2)
-        {
-          krs2.setPos(0, pos_r2); // 位置指令 任意
-          maeposr2 = pos_r2;
-        }
       }
     }
 
@@ -182,8 +131,6 @@ void loop()
       count_EMG++;
       if (count_EMG > 3000) // ダンプ試験はここを変える
       {
-        krs2.setPos(0, pos_o2);
-        maeposr2 = pos_o2;
         flag_EMG = 1;
       }
     }
@@ -218,22 +165,15 @@ void loop()
     {
       Serial.println("Open position selected");
       Serial.println(krs.setPos(0, pos_o));
-      Serial.println(krs2.setPos(0, pos_o2));
       Serial.println(krs.getPos(0));
-      Serial.println(krs2.getPos(0));
       kakudo_r = kakudo_o;
-      kakudo_r2 = kakudo_o2;
       maeposr = pos_o;
-      maeposr2 = pos_o2;
     }
     else if (input == 'c')
     {
       krs.setPos(0, pos_c);
-      krs2.setPos(0, pos_c2);
       kakudo_r = kakudo_c;
-      kakudo_r2 = kakudo_c2;
       maeposr = pos_c;
-      maeposr2 = pos_c2;
     }
   }
   delay(10);
